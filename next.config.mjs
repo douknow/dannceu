45775cf91a160584f739186994e9bf214ac5066d
cry <saynotta@gmail.com>
2024-09-13 04:15:18 +0800

    feat: cros

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    headers: [
+        {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+        },
+        {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+        }
+    ]
 };
 
 export default nextConfig; 
