c2952ca2ccee749c42bd5563ae94a4aa569a8a36
cry <saynotta@gmail.com>
2024-09-14 02:01:33 +0800

    feat: Polish canvas

--- src/app/components/Canvas.jsx
+++ src/app/components/Canvas.jsx
@@ -2,23 +2,26 @@ import React, { useState, useEffect } from 'react';
 
 const Canvas = React.forwardRef((props, ref) => {
   return (
-    <div className="bg-white m-auto absolute top-0 left-0"
+    <div className="bg-white m-auto absolute top-0 left-0 overflow-hidden"
         style={{
             width: props.width + 'px',
-            height: props.height + 'px'
+            height: props.height + 'px',
+            maskImage: 'linear-gradient(to bottom, white, white 100%)'
         }}
         ref={ref}>
-        <img src={props.avatar} alt="" className='w-[64px] h-[64px] rounded-[6px] absolute block bg-white' style={{
+        <img src={props.avatar} alt="" className='rounded-[6px] fixed block bg-white' style={{
           left: '16px',
-          top: 16 + 6 + 'px'
+          top: 16 + 6 + 'px',
+          width: 64,
+          height: 64
         }} />
 
-        <div className='text-[17px] absolute block text-[#3c3c4399]' style={{
+        <div className='text-[17px] fixed block text-[#3c3c4399]' style={{
           left: 16 + 64 + 20 + 'px',
           top: 16 + 'px'
         }} >{props.nickname}</div>
 
-        <img src={props.contentEmoji} alt="" className='w-[600px] h-[400px] absolute block' style={{
+        <img src={props.contentEmoji} alt="" className='fixed block' style={{
             left: props.emojiRect.left,
             top: props.emojiRect.top,
             width: props.emojiRect.width,
